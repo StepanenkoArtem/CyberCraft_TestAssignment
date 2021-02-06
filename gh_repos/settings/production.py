47320e7e9b9c815803c4071db70715f4355f48e6
Artem Stepanenko <artem@stepanenko.one>
2021-02-06 20:49:48 +0200

    add ALLOWED_HOSTS setting getting from ENV VAR

--- gh_repos/settings/production.py
+++ gh_repos/settings/production.py
@@ -4,4 +4,6 @@ from gh_repos.settings.common import *
 
 rollbar.report_message('Start production server', level='INFO')
 
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS')
+
 django_heroku.settings(locals())  # noqa: WPS421
