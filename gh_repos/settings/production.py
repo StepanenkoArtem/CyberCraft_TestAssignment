05fb80b6c366847cebd12f1cca27c1225a2d8e50
Artem Stepanenko <artem@stepanenko.one>
2021-02-06 20:14:39 +0200

    add django_heroku invokations in productions

--- gh_repos/settings/production.py
+++ gh_repos/settings/production.py
@@ -1,16 +1,5 @@
+import django_heroku
+
 from gh_repos.settings.common import *
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
-
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'HOST': 'localhost',
-        'PORT': os.getenv('GH_REPOS_DB_PORT'),
-        'USER': os.getenv('GH_REPOS_DB_USER'),
-        'PASSWORD': os.getenv('GH_REPOS_DB_PASS'),
-    },
-}
+django_heroku.settings(locals())  # noqa: WPS421
