15c009c5a88c54c7231359cb8213ac571daaa50d
Artem Stepanenko <artem@stepanenko.one>
2021-02-06 22:08:11 +0200

    update staticfiles settings

--- gh_repos/settings/common.py
+++ gh_repos/settings/common.py
@@ -115,7 +115,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 ROLLBAR = {
