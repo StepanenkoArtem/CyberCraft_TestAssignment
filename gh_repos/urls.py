3ef55615ed150395850c4f38b045d00f8e934468
Artem Stepanenko <artem@stepanenko.one>
2021-02-06 20:58:17 +0200

    add simple view controller for index page

--- gh_repos/urls.py
+++ gh_repos/urls.py
@@ -21,6 +21,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from gh_repos.views import IndexView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', IndexView.as_view()),
 ]
