ba06195b94fe9118ad2fb72014dd1d4b36b1e64d
Artem Stepanenko <artem@stepanenko.one>
2021-02-06 14:42:38 +0200

    replace os.getenv() to environ.get()

--- gh_repos/settings/common.py
+++ gh_repos/settings/common.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('GH_REPOS_SECRET_KEY')
+SECRET_KEY = os.environ.get('GH_REPOS_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('GH_REPOS_DEBUG_MODE')
+DEBUG = os.environ.get('GH_REPOS_DEBUG_MODE')
 
 # Application definition
 
